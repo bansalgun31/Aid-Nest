ff4b4c8360a3d23f774e3978c6d294e58bc9859c
bansalgun31 <155285707+bansalgun31@users.noreply.github.com>
2025-08-06 08:32:56 +0530

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/About";
-import Pages from "./Components/Pages";
-import Blogs from "./Components/Blogs";
-import Causes from "./Components/Causes";
-import Contact from "./Components/Contact";
-import Volunteer from "./Components/Volunteer";
+import Home from "./Components/Home.jsx";
+import About from "./Components/About.jsx";
+import Pages from "./Components/Pages.jsx";
+import Blogs from "./Components/Blogs.jsx";
+import Causes from "./Components/Causes.jsx";
+import Contact from "./Components/Contact.jsx";
+import Volunteer from "./Components/Volunteer.jsx";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import {
